ed22ce6fe9c0485d35ddde09e73caa2df456ebe1
Rudi Wijaya <rudi.mail90@gmail.com>
2019-01-21 07:48:59 +0700

    passing props

--- src/hoc/WithClassV2.js
+++ src/hoc/WithClassV2.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+// const withClassV2 = (WrappedComponent, upClassName) => {
+//     return (props) => (
+//         <div className={upClassName}>
+//             <WrappedComponent {...props} />
+//         </div>
+//     )
+// }
 
 const withClassV2 = (WrappedComponent, upClassName) => {
-    return (props) => (
-        <div className={upClassName}>
-            <WrappedComponent />
+    return class extends Component {
+      render() {
+        return (
+            <div className={upClassName}>
+            <WrappedComponent {...this.props} />
         </div>
-    )
+        )
+      }  
+    }
 }
 
 export default withClassV2;
